20b4324c8113158ab2893729efd6f29c238caceb
Krishna241142 <krishnabacchuwar7@gmail.com>
2024-06-17 11:08:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,46 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Custom Smoothe Order :cup_with_straw:")
+st.write("""Orders that need to filled.""")
+
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+editable_df = st.experimental_data_editor(my_dataframe)
+
+
+
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Custom Smoothe Order :cup_with_straw:")
+st.write("""Orders that need to filled.""")
+
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+editable_df = st.experimental_data_editor(my_dataframe)
+
+
+submitted = st.button('submit')
+
+if submitted:
+    st.success("Somwone clicked the button", icon = "👍")
+
+
+
+
+
+
+
+
 
