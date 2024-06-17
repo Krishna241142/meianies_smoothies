423d115e32b677efe8ae8e5447eb5aac73e5a888
Krishna241142 <krishnabacchuwar7@gmail.com>
2024-06-17 11:13:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,51 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Custom Smoothe Order :cup_with_straw:")
-st.write("""Orders that need to filled.""")
+st.write(
+    """Coose the fruits you want in custom smoothie!
+    """)
 
 
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-editable_df = st.experimental_data_editor(my_dataframe)
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
 
 
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
-# Import python packages
-import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    )
+if ingredients_list:
+    ingredients_string = ''
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
-# Write directly to the app
-st.title(":cup_with_straw: Custom Smoothe Order :cup_with_straw:")
-st.write("""Orders that need to filled.""")
+    
+    
+    
+    
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """')"""
 
+    
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
 
-
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-editable_df = st.experimental_data_editor(my_dataframe)
-
-
-submitted = st.button('submit')
-
-if submitted:
-    st.success("Somwone clicked the button", icon = "👍")
-
-
-
-
+        st.success('Your Smoothie is ordered!', icon="✅")
+        
+        
 
 
 
